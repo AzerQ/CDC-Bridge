830036bfdde6e28001e87b9aa567e53b068080c0
a.vorobev901 <a.vorobev901@kbptula.ru>
2025-12-25 14:35:22 +0300

    Добавлен скрипт для отключения CDC для таблицы

---
+++ .devcontainer/mssql/disable_cdc_for_table.sql
@@ -0,0 +1,5 @@
+﻿EXEC sys.sp_cdc_disable_table @source_schema = N'dbo', @source_name = N'TableName', @capture_instance = N'all'
+
+exec sp_executesql N'DECLARE @SchemaID INT = SCHEMA_ID(''dbo'')
+SELECT TOP 1 is_tracked_by_cdc FROM sys.tables
+WHERE schema_id = @SchemaID AND name = @TableName',N'@TableName nvarchar(4000)',@TableName=N'TableName'
